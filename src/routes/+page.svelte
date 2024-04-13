d7c2537bb622d9429f2ad65536c419a2a20001e7
Marwaxhello <marwahello33@gmail.com>
2024-04-14 00:21:56 +0200

    Renamed component import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,9 @@
-
 <script>
-  import Header from "$lib/componenten/header.svelte";
-  import Lessons from "../lib/componenten/lessons.svelte";
-  import Love from "../lib/componenten/love.svelte";
-  import Footer from "../lib/componenten/footer.svelte";
+	import Header from '$lib/componenten/header.svelte';
+	import Footer from '../lib/componenten/footer.svelte';
+	import Lectures from '../lib/componenten/lectures.svelte';
 </script>
 
 <Header />
-<Lessons />
-<Love />
+<Lectures />
 <Footer />
